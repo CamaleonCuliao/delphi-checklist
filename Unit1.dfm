e43b05c7952190d95d3ce73123e0433853635602
Borjaaula <gambasconarroz12345@gmail.com>
2026-04-22 12:58:01 +0200

    intento de arreglo
    punto pas y dfm

--- Unit1.dfm
+++ Unit1.dfm
@@ -2,8 +2,8 @@ object Form1: TForm1
   Left = 0
   Top = 0
   Caption = 'Delphi Checklist'
-  ClientHeight = 441
-  ClientWidth = 624
+  ClientHeight = 688
+  ClientWidth = 1132
   Color = clBtnFace
   Font.Charset = DEFAULT_CHARSET
   Font.Color = clWindowText
@@ -13,33 +13,72 @@ object Form1: TForm1
   Menu = MainMenu1
   OnCreate = FormCreate
   TextHeight = 15
-  object TreeView1: TTreeView
-    Left = 0
-    Top = 0
-    Width = 624
-    Height = 441
-    Align = alClient
-    Indent = 19
-    TabOrder = 0
+  object anadir: TSpeedButton
+    Left = 712
+    Top = 264
+    Width = 320
+    Height = 33
+    Caption = 'A'#241'adir'
+  end
+  object eliminar: TSpeedButton
+    Left = 712
+    Top = 360
+    Width = 320
+    Height = 33
+    Caption = 'Eliminar'
   end
   object DBGrid1: TDBGrid
-    Left = 296
-    Top = 8
+    Left = 712
+    Top = 64
     Width = 320
     Height = 120
     DataSource = DataSource1
-    TabOrder = 1
+    TabOrder = 0
     TitleFont.Charset = DEFAULT_CHARSET
     TitleFont.Color = clWindowText
     TitleFont.Height = -12
     TitleFont.Name = 'Segoe UI'
     TitleFont.Style = []
   end
+  object Panel1: TPanel
+    Left = 16
+    Top = 8
+    Width = 625
+    Height = 657
+    Caption = 'Panel1'
+    Color = clBackground
+    ParentBackground = False
+    TabOrder = 1
+    object TreeView1: TTreeView
+      Left = 8
+      Top = 16
+      Width = 609
+      Height = 625
+      Indent = 19
+      TabOrder = 0
+    end
+  end
+  object crearlista: TBitBtn
+    Left = 712
+    Top = 24
+    Width = 121
+    Height = 25
+    Caption = 'Crear Lista'
+    TabOrder = 2
+  end
+  object borrarLista: TBitBtn
+    Left = 839
+    Top = 24
+    Width = 121
+    Height = 25
+    Caption = 'Borrar Lista'
+    TabOrder = 3
+  end
   object MainMenu1: TMainMenu
     BiDiMode = bdLeftToRight
     ParentBiDiMode = False
-    Left = 568
-    Top = 384
+    Left = 880
+    Top = 472
     object f1: TMenuItem
       Caption = 'API'
     end
@@ -50,30 +89,31 @@ object Form1: TForm1
   object FDConnection1: TFDConnection
     Params.Strings = (
       'User_Name=root'
-      'DriverID=MySQL'
       'Server=localhost'
-      'Database=delphi-checklist')
-    Left = 40
-    Top = 384
+      'Database=delphichecklist'
+      'Port=3307'
+      'DriverID=MySQL')
+    Left = 728
+    Top = 544
   end
   object FDQuery1: TFDQuery
     Connection = FDConnection1
     SQL.Strings = (
       'SELECT * FROM item')
-    Left = 128
-    Top = 384
+    Left = 816
+    Top = 544
   end
   object DataSource1: TDataSource
     DataSet = FDQuery1
-    Left = 208
-    Top = 384
+    Left = 896
+    Top = 544
   end
   object FDQuery2: TFDQuery
     Connection = FDConnection1
     SQL.Strings = (
       'SELECT * FROM item WHERE id_lista_padre = :id')
-    Left = 128
-    Top = 328
+    Left = 816
+    Top = 488
     ParamData = <
       item
         Name = 'ID'
--- Unit1.pas
+++ Unit1.pas
@@ -10,11 +10,10 @@ uses
   FireDAC.Stan.Async, FireDAC.Phys, FireDAC.VCLUI.Wait, Data.DB,
   FireDAC.Comp.Client, FireDAC.Phys.MySQL, FireDAC.Phys.MySQLDef,
   FireDAC.Stan.Param, FireDAC.DatS, FireDAC.DApt.Intf, FireDAC.DApt, Vcl.Grids,
-  Vcl.DBGrids, FireDAC.Comp.DataSet;
+  Vcl.DBGrids, FireDAC.Comp.DataSet, Vcl.ExtCtrls, Vcl.Buttons;
 
 type
   TForm1 = class(TForm)
-    TreeView1: TTreeView;
     MainMenu1: TMainMenu;
     f1: TMenuItem;
     dwaf1: TMenuItem;
@@ -23,6 +22,12 @@ type
     DataSource1: TDataSource;
     DBGrid1: TDBGrid;
     FDQuery2: TFDQuery;
+    Panel1: TPanel;
+    TreeView1: TTreeView;
+    anadir: TSpeedButton;
+    eliminar: TSpeedButton;
+    crearlista: TBitBtn;
+    borrarLista: TBitBtn;
     procedure FormCreate(Sender: TObject);
   private
     { Private declarations }
@@ -37,43 +42,40 @@ implementation
 
 {$R *.dfm}
 
-// Funcion de prueba local, no funciona con BDD
 procedure TForm1.FormCreate(Sender: TObject);
 var
-i: Integer;
+  i: Integer;
+  NodoPadre, NodoHijo: TTreeNode;   // Declaración correcta de variables locales
 begin
-FDConnection1.Connected := True;
-FDQuery1.Open;
+  FDConnection1.Connected := True;
+  FDQuery1.Open;
 
-WindowState := wsMaximized;
+  WindowState := wsMaximized;
 
-// Activar checkboxes en el TreeView
-TreeView1.CheckBoxes := True;
+  // Activar checkboxes en el TreeView
+  TreeView1.CheckBoxes := True;
 
-// Agregar nodos padre e hijo
-var
-  NodoPadre, NodoHijo: TTreeNode;
-begin
-
+  // Recorrer el dataset y construir el árbol
   FDQuery1.First;
   while not FDQuery1.EOF do
   begin
-
-    //Si el id_padre es 0, lo inserta (raiz del programa)
-    if  FDQuery1.FieldByName('id_lista_padre').AsInteger = 0 then
-      NodoPadre := TreeView1.Items.Add(nil, FDQuery1.FieldByName('texto').AsString)
+    // Verificar si es nodo raíz (id_lista_padre = 0)
+    if FDQuery1.FieldByName('id_lista_padre').AsInteger = 0 then
+    begin
+      NodoPadre := TreeView1.Items.Add(nil, FDQuery1.FieldByName('texto').AsString);
+      NodoPadre.Checked := False;   // Inicialmente desmarcado
+    end
     else
     begin
-
-      //Selecciona la columna padre de la BD
-      FDQuery2.SQL.Text := 'SELECT * FROM item WHERE id = :id';
+      // Obtener el texto del nodo padre desde la base de datos
+      FDQuery2.SQL.Text := 'SELECT texto FROM item WHERE id = :id';
       FDQuery2.Params.Clear;
-      FDQuery2.Params.Add.Name := 'id';          // Crear el parámetro
+      FDQuery2.Params.Add.Name := 'id';
       FDQuery2.ParamByName('id').AsInteger := FDQuery1.FieldByName('id_lista_padre').AsInteger;
       FDQuery2.Open;
       FDQuery2.First;
 
-      //La saca del TreeView y la asigna a la variable NodoPadre
+      // Buscar el nodo padre en el TreeView por su texto
       NodoPadre := nil;
       for i := 0 to TreeView1.Items.Count - 1 do
       begin
@@ -84,20 +86,21 @@ begin
         end;
       end;
 
-      // Insertar como hijo del padre encontrado
-      NodoHijo := TreeView1.Items.AddChild(NodoPadre, FDQuery1.FieldByName('texto').AsString);
+      // Si se encontró el padre, agregar como hijo; sino, agregar como raíz (por seguridad)
+      if NodoPadre <> nil then
+        NodoHijo := TreeView1.Items.AddChild(NodoPadre, FDQuery1.FieldByName('texto').AsString)
+      else
+        NodoHijo := TreeView1.Items.Add(nil, FDQuery1.FieldByName('texto').AsString);
+
       NodoHijo.Checked := False;
-
       FDQuery2.Close;
-
     end;
-
 
     FDQuery1.Next;
   end;
 
-end;
-
+  // Expandir todos los nodos para visualización inmediata
+  TreeView1.FullExpand;
 end;
 
 end.
