fe90a3d97ddc06736b2189aebda9ecb86fb9df6e
CamaleonCuliao <alejandrogomezguillen9@gmail.com>
2026-04-23 09:58:36 +0200

    mover archivo Unit1

--- delphi-checklist-main/delphi_checklist.dpr
+++ delphi-checklist-main/delphi_checklist.dpr
@@ -5,7 +5,8 @@ uses
   Unit1 in '..\Unit1.pas' {Form1},
   Vcl.Themes,
   Vcl.Styles,
-  Unit3 in 'Unit3.pas' {dm_data: TDataModule};
+  Unit3 in 'Unit3.pas' {dm_data: TDataModule},
+  Unit2 in 'Unit2.pas' {dm_logic: TDataModule};
 
 {$R *.res}
 
@@ -13,7 +14,8 @@ begin
   Application.Initialize;
   Application.MainFormOnTaskbar := True;
   TStyleManager.TrySetStyle('Glossy');
-  Application.CreateForm(TForm1, Form1);
   Application.CreateForm(Tdm_data, dm_data);
+  Application.CreateForm(TForm1, Form1);
+  Application.CreateForm(Tdm_logic, dm_logic);
   Application.Run;
 end.
