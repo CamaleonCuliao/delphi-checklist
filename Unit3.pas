a7c3aac483ba5b43873b36f0426d5ed8139adb5e
Pau Lledó <lledolopezpau@gmail.com>
2026-04-28 17:03:54 +0200

    Funcionalidades botones proyectos menos Borrar

---
+++ Unit3.pas
@@ -0,0 +1,41 @@
+unit Unit3;
+
+interface
+
+uses
+  System.SysUtils, System.Classes, FireDAC.Stan.Intf, FireDAC.Stan.Option,
+  FireDAC.Stan.Error, FireDAC.UI.Intf, FireDAC.Phys.Intf, FireDAC.Stan.Def,
+  FireDAC.Stan.Pool, FireDAC.Stan.Async, FireDAC.Phys, FireDAC.Phys.MySQL,
+  FireDAC.Phys.MySQLDef, FireDAC.VCLUI.Wait, FireDAC.Stan.Param, FireDAC.DatS,
+  FireDAC.DApt.Intf, FireDAC.DApt, Vcl.Menus, Data.DB, FireDAC.Comp.DataSet,
+  FireDAC.Comp.Client;
+
+type
+  Tdm_data = class(TDataModule)
+    FDConnection1: TFDConnection;
+    FDQuery1: TFDQuery;
+    FDQuery2: TFDQuery;
+    DataSource1: TDataSource;
+    FDQuery3: TFDQuery;
+    FDQuery4: TFDQuery;
+    FDQuery5: TFDQuery;
+    FDQuery6: TFDQuery;
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  dm_data: Tdm_data;
+  IdUsuarioActual: Integer = 0;
+  IdListaActual: Integer = 0;
+
+implementation
+
+{%CLASSGROUP 'Vcl.Controls.TControl'}
+
+{$R *.dfm}
+
+
+end.
